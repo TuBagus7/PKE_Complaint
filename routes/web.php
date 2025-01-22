5079b51e754fd33876c7113289afb2833c746fc9
Tu Bagus Dwi Fikri <tubagusoseng@gmail.com>
2025-01-22 13:05:32 +0700

    Best Code Upload anda add function middleware

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,16 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Admin\DashboardController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/login', [LoginController::class, 'index'])->name('auth.login');
-Route::post('/login', [LoginController::class, 'store'])->name('auth.login.store');
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'store'])->name('login.store');
 
-
-//ketika kita mengakses aplikasi pertama kali akan di arahkan ke route /admin/dashboard
-Route::prefix('admin')->name('admin.')->group(function () {
-    Route::get('/dashboard', function () {
-        return view('pages.admin.dashboard');
-    })->name('dashboard');
+Route::prefix('admin')->name('admin')->middleware(['auth', 'role:admin'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 });
