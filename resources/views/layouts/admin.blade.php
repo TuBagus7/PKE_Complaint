2b974270bd8e2e6e68547c61f5c3a535672f90d9
Tu Bagus Dwi Fikri <tubagusoseng@gmail.com>
2025-01-30 12:49:35 +0700

    Show Data Report Finished

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -2,7 +2,6 @@
 <html lang="en">
 
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -10,20 +9,22 @@
     <meta name="author" content="">
 
     <title>@yield('title')</title>
+
+    <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
 
-    <!-- Custom fonts for this template-->
+    <!-- Custom fonts for this template -->
     <link href="{{ asset('assets/admin/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
-    <!-- Custom styles for this template-->
+    <!-- Custom styles for this template -->
     <link href="{{ asset('assets/admin/css/sb-admin-2.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/admin/vendor/datatables/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
 
-    <link href="{{asset('assets/admin/vendor/datatables/dataTables.bootstrap4.min.css')}}" rel="stylesheet">
+    <!-- Leaflet CSS -->
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" />
 
 </head>
 
@@ -35,7 +36,6 @@
         <!-- Sidebar -->
         @include('includes.sidebar')
         <!-- End of Sidebar -->
-
 
         <!-- Content Wrapper -->
         <div id="content-wrapper" class="d-flex flex-column">
@@ -67,22 +67,19 @@
     </div>
     <!-- End of Page Wrapper -->
 
-    <!-- Scroll to Top Button-->
+    <!-- Scroll to Top Button -->
     <a class="scroll-to-top rounded" href="#page-top">
         <i class="fas fa-angle-up"></i>
     </a>
 
-    <!-- Logout Modal-->
-    
-
-    <!-- Bootstrap core JavaScript-->
+    <!-- Bootstrap core JavaScript -->
     <script src="{{ asset('assets/admin/vendor/jquery/jquery.min.js') }}"></script>
     <script src="{{ asset('assets/admin/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 
-    <!-- Core plugin JavaScript-->
+    <!-- Core plugin JavaScript -->
     <script src="{{ asset('assets/admin/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
 
-    <!-- Custom scripts for all pages-->
+    <!-- Custom scripts for all pages -->
     <script src="{{ asset('assets/admin/js/sb-admin-2.min.js') }}"></script>
 
     <!-- Page level plugins -->
@@ -92,11 +89,16 @@
     <script src="{{ asset('assets/admin/js/demo/chart-area-demo.js') }}"></script>
     <script src="{{ asset('assets/admin/js/demo/chart-pie-demo.js') }}"></script>
 
-    <script src="{{asset('assets/admin/vendor/datatables/jquery.dataTables.min.js')}}"></script>
-    <script src="{{asset('assets/admin/vendor/datatables/dataTables.bootstrap4.min.js')}}"></script>
+    <script src="{{ asset('assets/admin/vendor/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('assets/admin/vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
 
-    <script src="{{asset('assets/admin/js/demo/datatables-demo.js')}}"></script>    
+    <script src="{{ asset('assets/admin/js/demo/datatables-demo.js') }}"></script>
 
+    <!-- Leaflet JS -->
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
+
+    <!-- Tempat untuk Script Tambahan -->
+    @yield('script')
 
 </body>
 
--- resources/views/pages/admin/report/show.blade.php
+++ resources/views/pages/admin/report/show.blade.php
@@ -1,29 +1,74 @@
 @extends('layouts.admin')
 
-@section('title', 'Detail Kategori Laporan')
+@section('title', 'Detail Laporan')
 
 @section('content')
     <!-- Page Heading -->
-    <a href="{{route('admin.category.index')}}" class="btn btn-danger mb-3">Kembali</a>
-
+    <a href="{{ route('admin.report.index') }}" class="btn btn-danger mb-3">Kembali</a>
 
     <!-- DataTales Example -->
     <div class="card shadow mb-4">
         <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary">Detail Kategori Laporan</h6>
+            <h6 class="m-0 font-weight-bold text-primary">Detail Laporan</h6>
         </div>
         <div class="card-body">
             <table class="table table-bordered">
                 <tr>
-                    <td>Nama</td>
-                    <td>{{$category->name}}</td>
+                    <td>Kode Laporan</td>
+                    <td>{{ $report->code }}</td>
                 </tr>
                 <tr>
-                    <td>Icon</td>
-                    <td><img src="{{asset('storage/' . $category->image)}}" alt="image" width="300"></td>
+                    <td>Pelapor</td>
+                    <td>{{ $report->resident->user->email }} - {{ $report->resident->user->name }}</td>
+                </tr>
+                <tr>
+                    <td>Kategori Laporan</td>
+                    <td>{{ $report->reportCategory->name }}</td>
+                </tr>
+                <tr>
+                    <td>Judul</td>
+                    <td>{{ $report->title }}</td>
+                </tr>
+                <tr>
+                    <td>Deskripsi Laporan</td>
+                    <td>{{ $report->description }}</td>
+                </tr>
+                <tr>
+                    <td>Bukti Laporan</td>
+                    <td><img src="{{ asset('storage/' . $report->image) }}" alt="image" width="300"></td>
+                </tr>
+                <tr>
+                    <td>Latitude</td>
+                    <td>{{ $report->latitude }}</td>
+                </tr>
+                <tr>
+                    <td>Longitude</td>
+                    <td>{{ $report->longitude }}</td>
+                </tr>
+                <tr>
+                    <td>Map</td>
+                    <td>
+                        <div id="map" style="height: 400px"></div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Alamat Laporan</td>
+                    <td>{{ $report->address }}</td>
                 </tr>
             </table>
         </div>
     </div>
+@endsection
 
+@section('script')
+<script>
+    var mymap = L.map('map').setView([{{ $report->latitude }}, {{ $report->longitude }}], 13);
+    
+    var marker = L.marker([{{ $report->latitude }}, {{ $report->longitude }}]).addTo(mymap);
+    
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+        maxZoom: 18,
+    }).addTo(mymap);
+</script>
 @endsection
